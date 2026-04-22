bbb09f168525ff3e53f5ed129b729549b5c20fc0
Petr Tikhomirov <tikhomirov10@mail.ru>
2026-04-22 22:21:30 +0300

    add new files

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-def attack(char_name, char_class):
+def attack(char_name: str, char_class: str):
     if char_class == 'warrior':
         return (f'{char_name} нанёс урон противнику '
                 f'равный {5 + randint(3, 5)}')
