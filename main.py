e9e966c9e6cb3da75703cd97d95873a5f03c36f3
Petr Tikhomirov <tikhomirov10@mail.ru>
2026-04-26 00:33:21 +0300

    добавлены файлы

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from random import randint
-
+from start_game_banner import run_screensaver
 
 def attack(char_name: str, char_class: str):
     if char_class == 'warrior':
@@ -82,15 +82,16 @@ def choice_char_class():
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
-    char_name = input('...назови себя: ')
+    char_name: str = input('...назови себя: ')
     print(f'Здравствуй, {char_name}! '
           'Сейчас твоя выносливость — 80, атака — 5 и защита — 10.')
     print('Ты можешь выбрать один из трёх путей силы:')
     print('Воитель, Маг, Лекарь')
-    char_class = choice_char_class()
+    char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
 
 
